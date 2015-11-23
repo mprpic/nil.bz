77e9de65f77d71b3dd629ed9c18c3cb752bdcce6
Martin Prpič <martin.prpic@gmail.com>
2015-11-23 23:41:57 +0100

    Allow trailing slashes

--- app/app.py
+++ app/app.py
@@ -12,31 +12,31 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/about')
+@app.route('/about', strict_slashes=False)
 def about():
     return render_template("soon.html")
     # return render_template("about.html")
 
-@app.route('/blog')
+@app.route('/blog', strict_slashes=False)
 def blog():
     return render_template("soon.html")
     # return render_template("blog.html")
 
-@app.route('/climbing-log')
+@app.route('/climbing-log', strict_slashes=False)
 def climbing():
     return render_template("soon.html")
     # remove function: static
 
-@app.route('/ferrata-log')
+@app.route('/ferrata-log', strict_slashes=False)
 def ferrata():
     return render_template("soon.html")
     # remove function: static
 
-@app.route('/photos')
+@app.route('/photos', strict_slashes=False)
 def photos():
     return render_template("photos.html")
 
-@app.route('/projects')
+@app.route('/projects', strict_slashes=False)
 def projects():
     return render_template("soon.html")
     # return render_template("projects.html")
