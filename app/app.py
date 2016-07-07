ce19e1e4e8fb0f024213f800fb2bc160ca32df3d
Martin Prpič <martin.prpic@gmail.com>
2016-07-07 23:09:56 +0200

    Add link to Norway pics
    Also clean up code formatting a bit and add countries to photo
    album titles.

--- app/app.py
+++ app/app.py
@@ -16,33 +16,39 @@ APP_DIR = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
+
 def verify_hash(request_body, header_value):
     h = hmac.new(config.repo_secret, request_body, hashlib.sha1)
     return hmac.compare_digest(bytes('sha1=' + h.hexdigest()),
                                bytes(header_value))
 
+
 def blog_post_url(post):
     print post
     return post.rstrip('.md').split('_')[1].lower()
 
+
 def blog_post_title(post):
     return post.rstrip('.md').split('_')[1].replace('-', ' ')
+
 
 @app.route('/')
 @app.route('/index.html')
 def home():
     return render_template('home.html')
 
+
 @app.route('/about', strict_slashes=False)
 def about():
     return render_template('about.html')
 
+
 @app.route('/blog/<post>', strict_slashes=False)
 def blog_posts(post):
     # Return posts that start with a date; those that don't are drafts
-    posts = [ f for f in os.listdir(APP_DIR + '/blog_posts') if f[0:7].isdigit() ]
+    posts = [f for f in os.listdir(APP_DIR + '/blog_posts') if f[0:7].isdigit()]
     # Contert post file names into URL IDs
-    stripped_posts = [ blog_post_url(i) for i in posts ]
+    stripped_posts = [blog_post_url(i) for i in posts]
 
     if post in stripped_posts:
         p = posts[stripped_posts.index(post)]
@@ -61,27 +67,30 @@ def blog_posts(post):
     else:
         return render_template('404.html'), 404
 
+
 @app.route('/blog', strict_slashes=False)
 def blog():
     # We can sort (newest first) alphabetically since we're using YYYYMMDD
-    posts = sorted([ f for f in os.listdir(APP_DIR + '/blog_posts') if
-                     f[0:7].isdigit() ], reverse=True)
-    post_urls = [ blog_post_url(p) for p in posts ]
+    posts = sorted([f for f in os.listdir(APP_DIR + '/blog_posts') if
+                    f[0:7].isdigit()], reverse=True)
+    post_urls = [blog_post_url(p) for p in posts]
 
-    post_titles = [ '[{}]  {}'.format(datetime.strptime(p.split('_')[0], '%Y%m%d') \
-                                              .strftime('%b %d, %Y'), \
-                                      blog_post_title(p)) for p in posts ]
+    post_titles = ['[{}]  {}'.format(datetime.strptime(p.split('_')[0], '%Y%m%d').strftime('%b %d, %Y'),
+                                     blog_post_title(p)) for p in posts]
 
     return render_template('blog.html', posts=zip(post_urls, post_titles))
+
 
 @app.route('/photos', strict_slashes=False)
 def photos():
     return render_template('photos.html')
 
+
 @app.route('/projects', strict_slashes=False)
 def projects():
     return render_template('soon.html')
     # return render_template('projects.html')
+
 
 @app.route('/regenerate-climbing-logs', methods=['POST'])
 def regen_logs():
@@ -106,6 +115,7 @@ def regen_logs():
         shutil.copy(config.climbing_repo + 'css/log.css', config.web_dir)
 
     return redirect(url_for('home'), code=302)
+
 
 @app.errorhandler(404)
 def not_found(e):
